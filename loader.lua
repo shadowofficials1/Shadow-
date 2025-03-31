2502abd806b53e845953a057245f59e74ccbb076
shadowofficials1 <shadowofficials100@gmail.com>
2025-03-31 20:35:39 +0800

    Delete loader.lua

--- loader.lua
+++
@@ -1,8 +0,0 @@
-local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/shadowofficials1/Shadow-/refs/heads/main/Game%20List.lua"))()
-
-for PlaceID, Execute in pairs(Games) do
-if PlaceID == game.PlaceId then
-loadstring(game:HttpGet(Execute))()
-end
-end
-
