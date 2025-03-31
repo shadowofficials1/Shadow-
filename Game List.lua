2ffdd15db98638ea012fb78707d0de5d5be72f4c
shadowofficials1 <shadowofficials100@gmail.com>
2025-03-31 20:23:05 +0800

    Update Game List.lua

--- Game List.lua
+++ Game List.lua
@@ -1,5 +1,5 @@
 local Games = {
-  [2753915549] = "Game raw", 
+  [4483381587] = "https://raw.githubusercontent.com/shadowofficials1/Shadow-/refs/heads/main/A%20literal%20Baseplate/4483381587.lua", -- a literal baseplate 
 }
 
 return Games
