4254ef0169de6bedb5e7e86ab29173204048fd61
shadowofficials1 <shadowofficials100@gmail.com>
2025-03-31 18:52:48 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,4 +1,4 @@
-local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/Shadow_officials1/Shadow/main/GameList.lua"))()
+local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/shadowofficials1/Shadow-/refs/heads/main/Game%20List.lua"))()
 
 for PlaceID, Execute in pairs(Games) do
 if PlaceID == game.PlaceId then
